81e93e72feaca5787647b6344b072c2db86fdba8
Happy9Hub <pantharee.saang@gmail.com>
2026-05-13 18:13:54 +0700

    shadcn/ui

--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,11 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import Link from "next/link";
 
 // https://localhost:3000/about
 export default function AboutPage() {
   return (
     <div>
-      <p>About Us</p>
-      <Link href="/">Home</Link>
+      <p className="text-sky-500">About Us</p>
+      <Spinner className="w-6 h-6" />
+      <Button variant="link"><Link href="/">Home</Link></Button>
+      
     </div>
   );
 }
