7d06cdf82e570d53ffe91eb428fd6ba1aea09d1a
Happy9Hub <pantharee.saang@gmail.com>
2026-06-10 10:40:28 +0700

    feat: integrate Prisma with MariaDB and set up database schema
    - Added Prisma dependencies to package.json for MariaDB support.
    - Created prisma.config.ts for Prisma configuration with environment variables.
    - Defined database schema in prisma/schema.prisma with models for categories, customers, orders, order_items, products, and product_images.
    - Implemented Prisma client initialization in src/lib/prisma.ts with connection settings.

---
+++ src/lib/prisma.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: Number(process.env.DATABASE_PORT),
+  connectionLimit: 5, // Adjust the connection limit as needed for production environments recommended to be >= 10
+});
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, 
+  }); 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+
+export { prisma };
