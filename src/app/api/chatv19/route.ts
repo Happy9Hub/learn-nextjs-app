c0a20e1a56c0ede14ed409ed9fabc90f38f45bcd
Happy9Hub <pantharee.saang@gmail.com>
2026-07-08 18:06:22 +0700

    feat: enhance chat functionality with PostgreSQL integration and session management

--- src/app/api/chatv19/route.ts
+++ src/app/api/chatv19/route.ts
@@ -4,6 +4,11 @@ import { createAgent } from "langchain";
 import { createUIMessageStreamResponse, UIMessage } from "ai";
 import { toBaseMessages, toUIMessageStream } from "@ai-sdk/langchain";
 import { getCurrentDateTool, searchAllProductTool } from "@/agent-tools";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { getCheckpointer } from "@/db/checkpointer";
+import { prisma } from "@/lib/prisma";
 
 const llmModel = new ChatOllama({
     model: 'gemma4:31b-cloud',
@@ -13,23 +18,59 @@ const llmModel = new ChatOllama({
 });
 
 export async function POST(req: NextRequest) {
-    const { messages }: { messages: UIMessage[] } = await req.json();
+    const session = await auth.api.getSession({
+        headers: await headers()
+      });
+    
+      if (!session) {
+        redirect("/sign-in");
+      }
+      
+    const { messages, sessionId }: { messages: UIMessage[], sessionId: string } = await req.json();
 
-    const langchainMessages = await toBaseMessages(messages);
+    // ส่งให้ agent เฉพาะข้อความทีล่าสุดของผู้ใช้เท่านั้น
+    const lastUserMessage = [...messages].reverse().find((message) => message.role === "user");
+    const [lastLangchainMessage] = lastUserMessage ? await toBaseMessages([lastUserMessage]) : [];
+
+    // shortterm memory for the agent
+    const checkpointer = await getCheckpointer();
 
     // create AI Agent
     const agent = createAgent({
         model: llmModel,
         systemPrompt: `คุณเป็นผู้ช่วยสนับสนุนลูกค้า AI ที่มีความรู้เกี่ยวกับผลิตภัณฑ์และบริการของบริษัทของเรา
-                    คุณจะตอบคำถามของลูกค้าและให้ข้อมูลที่ถูกต้องและเป็นประโยชน์ ให้ข้อมูลเกี่ยวกับวันและเวลาปัจจุบันเมื่อถูกถาม
-                    **ห้ามตอบเรื่องอื่นที่ไม่เกี่ยวข้อง**`,
+                    คุณจะตอบคำถามของลูกค้าและให้ข้อมูลที่ถูกต้องและเป็นประโยชน์
+                    รหัสลูกค้า: ${session.user.id}, ชื่อลูกค้า: ${session.user.name}
+                    ให้ข้อมูลเกี่ยวกับวันและเวลาปัจจุบันเมื่อถูกถาม`,
         tools: [ getCurrentDateTool, searchAllProductTool ],
+        checkpointer: checkpointer,
     });
 
     const response = agent.streamEvents(
-      { messages:  langchainMessages },
-      { version: "v2" }
+      { messages:  lastLangchainMessage  },
+      { streamMode: ['messages'], configurable:{thread_id: sessionId} }
     );
-    
-    return createUIMessageStreamResponse({ stream: toUIMessageStream(response)});
+
+    const uiMessageStream = toUIMessageStream(response, {
+        onFinal: async () => {
+            const previewText = JSON.stringify(lastLangchainMessage.content.slice(0, 50) ?? '');
+            console.log(previewText);
+            const res = await prisma.chatThread.upsert({
+                where: { threadId: sessionId },
+                update: {
+                    preview: previewText,
+                    messageCount: { increment: 2 },
+                    updatedAt: new Date()
+                },
+                create: {
+                    threadId: sessionId,
+                    userId: session.user.id,
+                    preview: previewText,
+                    messageCount: 2
+                }
+            });
+            console.log(res.threadId);
+        }
+    });
+    return createUIMessageStreamResponse({ stream: uiMessageStream });
 }
